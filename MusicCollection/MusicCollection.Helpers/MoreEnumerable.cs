0fbe61dac2791cc07fd1a3f730d17a64f3083817
Urado <Samurai-94@mail.ru>
2018-12-11 00:40:17 +0300

    Начало!

---
+++ MusicCollection/MusicCollection.Helpers/MoreEnumerable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MusicCollection.Helpers
+{
+    public static class MoreEnumerable
+    {
+        public static IReadOnlyList<T> ToReadOnlyList<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable.ToList();
+        }
+    }
+}
